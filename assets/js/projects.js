181809f931d43abe09e4cba7b1746ac4d4f792ac
Stefania Avallone <stefania.avallone@ericsson.com>
2022-02-06 20:27:03 +0100

    New CV and new popup in projects

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -11,16 +11,13 @@ $('#modalProject').on('show.bs.modal', function(e) {
     $(e.currentTarget).find('#tecnologiesProject').text($(e.relatedTarget).data('tecnologies-project'));
     $(e.currentTarget).find('#groupProject').text($(e.relatedTarget).data('group-project'));
     $(e.currentTarget).find('#dateProject').text($(e.relatedTarget).data('date-project'));
-    // $dataInterval = $(e.currentTarget).find('#carouselIndicators');
-    // $(e.currentTarget).find('#carouselIndicators').carousel('cycle')
 
     /* IFrame you tube */
     $src_frame = $(e.relatedTarget).data('frame-carousel')
     if ($src_frame != undefined) {
         $src_frame = $src_frame.split(',')
         for (i = 0; i < $src_frame.length; i++) {
-            $( "#carouselItem" ).append( " <div id=carouselItem_"+i+" class='carousel-item active'><iframe height="+ 345 +" class='iframe-size'  frameborder='"+0+"' allowfullscreen src='"+$src_frame[i]+"' /></div>" );
-            // $(e.currentTarget).find('#carouselIndicators').carousel('pause');
+            $( "#carouselItem" ).append( " <div id=carouselItem_"+i+" class='carousel-item active'><iframe height="+ 345 +" class='iframe-size  d-block w-100'  frameborder='"+0+"' controls='"+2+"' allowfullscreen='"+1+"' src='"+$src_frame[i]+"' /></div>" );
         }
     }
 
@@ -30,9 +27,9 @@ $('#modalProject').on('show.bs.modal', function(e) {
         $imgs = $imgs.split(',')
         for (i = 0; i < $imgs.length; i++) {
             if (i==0 && $src_frame == undefined) {
-                    $( "#carouselItem" ).append( "<div id=carouselItem_"+i+" class='carousel-item active'><img class='img-size' src='"+$imgs[i]+"'/></div>" );
+                    $( "#carouselItem" ).append( "<div id=carouselItem_"+i+" class='carousel-item active'><img class='img-size d-block w-100' src='"+$imgs[i]+"'/></div>" );
             } else {
-                $( "#carouselItem" ).append( " <div id=carouselItem_"+i+" class='carousel-item'><img class='img-size' src='"+$imgs[i]+"'/></div>" );
+                $( "#carouselItem" ).append( " <div id=carouselItem_"+i+" class='carousel-item'><img class='img-size d-block w-100' src='"+$imgs[i]+"'/></div>" );
             }        
         }
     }
