7bd3fd0efce0e6192f32bd3b89a0bd6fee6093fc
Stefania Avallone <11625506@telecomitalia.it>
2022-07-14 17:33:28 +0200

    modify send mail

--- assets/js/mail-form.js
+++ assets/js/mail-form.js
@@ -1,61 +1,43 @@
-$('#form-submit').click(function() {
-  $('#contact').validate({
-            highlight: function(input) {
-                $(input).parents('#form-line').addClass('form-error');
-            },
-            unhighlight: function(input) {
-                $(input).parents('#form-line').removeClass('form-success');
-            },
-            errorPlacement: function(error, element) {
-                $(element).parents('#form-line').append(error);
-            },
-            submitHandler: function(form) {
-                //Will execute only when the form passed validation.
-                OnSubmit(form);
-            }
-        });
-});
-
-
-function OnSubmit(form) {
+function sendMail() {
   subject = "Email from: " + $('#name').val();
   fromAddress = $('#from_address').val();
   toAddress = "stefaniaavallone3@gmail.com";
   body =  $('#body-message').val();
-  
   var myHeaders = new Headers();
   myHeaders.append("Accept", "*/*");
-  // myHeaders.append("Referer", "http://127.0.0.1:5500/?subject=Stefania+Avallone&from_address=crispogioele%40gmail.com&to_address=stefaniaavallone3%40gmail.com&body=sfd");
+  myHeaders.append("Referer", "http://127.0.0.1:5500/?subject=Stefania+Avallone&from_address=crispogioele%40gmail.com&to_address=stefaniaavallone3%40gmail.com&body=sfd");
   myHeaders.append("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36");
   myHeaders.append("Content-Type", "application/json");
-  let messageData = {
-    'from_address': fromAddress,
-    'to_address': toAddress,
-    'subject': subject,
-    'body': body
-}
-  $.ajax({
-      url: 'https://email-sender-protected.herokuapp.com/send',
-      dataType: 'json',
-      type: 'POST',
-      contentType: 'application/json',
-      processData: false,
-      data: {
-          body: messageData,
-      },
-      success: function(msg) {
-          $("#modalMail").text('Your email has been sent sucessfully');
-          $("#modalMail").modal('show');
-          setTimeout(function () {
-            console.log('hejsan');
-            $('.modal').modal('hide');
-          }, 3000);
-          $('#contact')[0].reset();
-          return false;
-      },
-      error: function(){
-        $("#modalMail").text('There were problems with your email. Try again later please!');
-        $("#modalMail").modal('show');
-      }
-    });
+  
+var raw = JSON.stringify({
+    "from_address": fromAddress,
+    "to_address": "stefaniaavallone3@gmail.com",
+    "subject":subject,
+    "body": body
+  });
+  
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+  
+  
+  fetch("https://email-sender-protected.herokuapp.com/send", requestOptions)
+    .then(response => response.text())
+    .then(result =>  onclick="modal();")
+  }
+
+function modal(){
+  let context = document.getElementById("modalMail");
+  var myModal = new bootstrap.Modal(context, {});
+  context.querySelector(".modal-body").innerHTML = "Successfully Delete Uesr with ID: ";
+  myModal.show();
+  setTimeout(function () {
+    let context = document.getElementById("modalMail");
+    var myModal = new bootstrap.Modal(context, {});
+        console.log('hejsan');
+        $('.modal').modal('hide');
+      }, 3000);
 }
